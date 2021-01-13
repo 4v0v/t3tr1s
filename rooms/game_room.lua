a831a208ff0254813e3c7444d92919a4753b769d
4v0v <30574115+4v0v@users.noreply.github.com>
2021-01-13 18:36:29 +0100

    ...

--- rooms/game_room.lua
+++ rooms/game_room.lua
@@ -10,8 +10,13 @@ function Game_room:new()
 		centered       = true,
 	}))
 
-	@.empty_grid = Grid(10, 20)
-	@.grid       = Grid(10, 20)
+	@.empty_grid    = Grid(10, 20)
+	@.grid          = Grid(10, 20)
+	@.current_idx   = 0
+	@.next_idx      = 0
+	@.next_piece    = {}
+	@.current_blocs = {}
+	@.placed_blocs  = {}
 
 	@.pieces = {
 		Grid(3, 3, {
@@ -20,63 +25,68 @@ function Game_room:new()
 			0, 0, 0,
 		}),
 		Grid(3, 3, {
-			2, 0, 0,
-			2, 2, 2,
+			1, 0, 0,
+			1, 1, 1,
 			0, 0, 0,
 		}), 
 		Grid(4, 4, {
 			0, 0, 0, 0,
-			3, 3, 3, 3,
+			1, 1, 1, 1,
 			0, 0, 0, 0,
 			0, 0, 0, 0
 		}),
 		Grid(3, 3, {
-			4, 4, 0,
-			0, 4, 4,
+			1, 1, 0,
+			0, 1, 1,
 			0, 0, 0,
 		}),
 		Grid(3, 3, {
-			0, 5, 5,
-			5, 5, 0,
+			0, 1, 1,
+			1, 1, 0,
 			0, 0, 0,
 		}),
 		Grid(3, 3, {
-			0, 6, 0,
-			6, 6, 6,
+			0, 1, 0,
+			1, 1, 1,
 			0, 0, 0,
 		}),
 		Grid(2, 2, {
-			7, 7,
-			7, 7,
+			1, 1,
+			1, 1,
 		}),
 	}
-
-	@.next_piece_idx    = 0
-	@.current_piece_idx = 0
-	@.next_piece        = {}
-	@.current_blocs     = {}
-	@.placed_blocs      = {}
-
-	@:generate_next()
-	@:every_immediate(.3, fn() @:move_down() end, _, 'move_down')
 end
 
 function Game_room:update(dt)
 	Game_room.super.update(@, dt)
 
-	if pressed('left') || pressed('q') then @:move_left() end
+	@:once(fn() 
+		@.next_idx = math.random(7)
+		local piece = @.pieces[@.next_idx]:to_table()
+		ifor bloc in piece do 
+			if bloc[3] != 0 then
+				insert(@.next_piece, {x = bloc[1], y = bloc[2], v = bloc[3]})
+			end
+		end
+
+		@:every_immediate(.3, fn() @:move_down() end, _, 'move_down')
+	end, 'update_grid')
+
+	if pressed('left')  || pressed('q') then @:move_left()  end
 	if pressed('right') || pressed('d') then @:move_right() end
-	if down('down') || down('s') then @:move_down() end
+	if down('down')     || down('s')    then @:move_down()  end
 end
 
 function Game_room:draw_outside_camera_fg()
 	Game_room.super.draw_outside_camera_fg(@)
 
+	-- grid
 	@.grid:foreach(fn(grid, i, j) 
 		lg.setColor(.4, .4, .4)
-		lg.rectangle("line", 25 * i, 25 * j, 25 , 25 )
+		lg.rectangle('line', 25 * i, 25 * j, 25 , 25 )
 	end)
 
+	-- blocks
 	@.grid:foreach(fn(grid, i, j)
 		local cell = grid:get(i, j)
 		if   cell == 0 then return
@@ -87,12 +97,13 @@ function Game_room:draw_outside_camera_fg()
 		elif cell == 5 then lg.setColor(CMYK(.4, .3, .2, .2))
 		elif cell == 6 then lg.setColor(CMYK(.5, .2, .1, .3))
 		elif cell == 7 then lg.setColor(CMYK(.7, .5, .2, .4))
-		elif cell == 8 then lg.setColor(CMYK(.5, .5, .5, .5)) end
-		lg.rectangle("fill", 25 * i, 25 * j, 25 , 25, 5, 5 )
+		elif cell == 8 then lg.setColor(CMYK(.3, .3, .3, .3)) end
+		lg.rectangle('fill', 25 * i, 25 * j, 25 , 25, 5, 5 )
 	end)
 
+	-- next piece preview
 	lg.setColor(.5, .5, .5)
-	lg.rectangle("line", 300, 25, 100, 100)
+	lg.rectangle('line', 300, 25, 100, 100)
 
 	ifor @.next_piece do 
 		if   it.v == 1 then lg.setColor(CMYK(.5, .3, .6, .1))
@@ -102,8 +113,8 @@ function Game_room:draw_outside_camera_fg()
 		elif it.v == 5 then lg.setColor(CMYK(.4, .3, .2, .2))
 		elif it.v == 6 then lg.setColor(CMYK(.5, .2, .1, .3))
 		elif it.v == 7 then lg.setColor(CMYK(.7, .5, .2, .4))
-		elif it.v == 8 then lg.setColor(CMYK(.5, .5, .5, .5)) end
-		lg.rectangle("fill", 287.5 + 25 * it.x , 25 + 25 * it.y, 25 , 25, 5, 5 )
+		elif it.v == 8 then lg.setColor(CMYK(.3, .3, .3, .3)) end
+		lg.rectangle('fill', 287.5 + 25 * it.x , 25 + 25 * it.y, 25 , 25, 5, 5 )
 	end
 end
 
@@ -121,6 +132,7 @@ function Game_room:move_down()
 			end
 			
 			-- check in placed blocs if complete lines
+			-- TODO this check multiple times
 			ifor @.current_blocs do 
 				if
 					@.grid:get(1,  it.y) == 8 &&
@@ -154,7 +166,25 @@ function Game_room:move_down()
 	end
 
 	if #@.current_blocs == 0 then
-		@:insert_current()
+		do -- generate new current && next piece
+			@.current_idx = @.next_idx
+			ifor @.next_piece do
+				if it.v != 0 then 
+					insert(@.current_blocs, {x = it.x + 3, y = it.y, v = @.next_idx}) 
+				end
+			end
+			@.next_piece = {}
+		
+			@.next_idx = math.random(7)
+			local piece = @.pieces[@.next_idx]:to_table()
+			ifor bloc in piece do 
+				if bloc[3] != 0 then
+					insert(@.next_piece, {x = bloc[1], y = bloc[2], v = bloc[3]})
+				end
+			end
+		
+			ifor @.current_blocs do @.grid:set(it.x, it.y, @.current_idx) end
+		end
 	end
 end
 
@@ -183,8 +213,8 @@ function Game_room:move_right()
 	ifor @.placed_blocs do @.grid:set(it.x, it.y, 8) end
 
 	ifor @.current_blocs do
-		local temp_x, temp_y = it.x + 1, it.y
-		if @.grid:is_oob(temp_x, temp_y) || @.grid:get(temp_x, temp_y) != 0 then 
+		local dx, dy = it.x + 1, it.y
+		if @.grid:is_oob(dx, dy) || @.grid:get(dx, dy) != 0 then 
 			goto cannot_move
 		end
 	end
@@ -200,27 +230,4 @@ function Game_room:rotate_clockwise()
 end
 
 function Game_room:rotate_anticlockwise()
-end
-
-function Game_room:insert_current()
-	@.current_piece_idx = @.next_piece_idx
-	ifor @.next_piece do
-		if it.v != 0 then 
-			insert(@.current_blocs, {x = it.x + 3, y = it.y, v = it.v}) 
-		end
-	end
-	@.next_piece = {}
-	@:generate_next()
-
-	ifor @.current_blocs do @.grid:set(it.x, it.y, it.v) end
-end
-
-function Game_room:generate_next()
-	@.next_piece_idx = math.random(7)
-	local piece = @.pieces[@.next_piece_idx]:to_table()
-	ifor bloc in piece do 
-		if bloc[3] != 0 then
-			insert(@.next_piece, {x = bloc[1], y = bloc[2], v = bloc[3]})
-		end
-	end
 end
